9d3d84dd970af02d5607551827f89eea809519c2
yy4551 <100992443+yy4551@users.noreply.github.com>
2024-01-22 09:44:15 +0800

    img，seg，pancreas_seg,tumor_seg,ctv_seg,完成

--- src/DicomRTTool/working_on_pancreas.py
+++ src/DicomRTTool/working_on_pancreas.py
@@ -54,12 +54,9 @@ for _,dcmfolder in enumerate(os.listdir(Dicom_path)):
     ctv_numpy = Dicom_reader.mask[1]
     pancreas_numpy = Dicom_reader.mask[2]
 
-
     tumor_size = np.count_nonzero(Dicom_reader.mask[0])
     ctv_size = np.count_nonzero(Dicom_reader.mask[1])
     pancreas_size = np.count_nonzero(Dicom_reader.mask[2])
-
-    print(f"tumor_size = {tumor_size},ctv_size = {ctv_size},pancreas_size = {pancreas_size}")
 
     if tumor_size > pancreas_size or ctv_size > pancreas_size:
         who_has_tiny_pancreas.append(dcmfolder)
@@ -67,18 +64,13 @@ for _,dcmfolder in enumerate(os.listdir(Dicom_path)):
     logger.info(f'{dcmfolder} : img_shape = {Dicom_reader.ArrayDicom.shape}, seg_shape = {Dicom_reader.mask.shape}'
                 f' tumor_size = {tumor_size},ctv_size = {ctv_size},pancreas_size = {pancreas_size}')
 
-
-    sitk.WriteImage(Dicom_reader.dicom_handle, os.path.join(img_path, 'img', dcmfolder,'.nii.gz'))
-    sitk.WriteImage(Dicom_reader.annotation_handle, os.path.join(seg_path, 'seg', dcmfolder,'.nii.gz'))
-    sitk.WriteImage(sitk.GetImageFromArray(pancreas_numpy), os.path.join(pancreas_seg_path, 'pancreas_seg', dcmfolder,'.nii.gz'))
-    sitk.WriteImage(sitk.GetImageFromArray(tumor_numpy), os.path.join(tumor_seg_path, 'tumor_seg', dcmfolder,'.nii.gz'))
-    sitk.WriteImage(sitk.GetImageFromArray(ctv_numpy), os.path.join(ctv_seg_path, 'ctv_seg', dcmfolder,'.nii.gz'))
-
-print(f"who_has_tiny_pancreas={who_has_tiny_pancreas}")
-print(f"no_pancreas = {no_pancreas}")
-print(f"mo_tumor = {no_tumor}")
-print(f"no_ctv = {no_ctv}")
+    sitk.WriteImage(Dicom_reader.dicom_handle, os.path.join(img_path, f'img_{dcmfolder}.nii.gz'))
+    sitk.WriteImage(Dicom_reader.annotation_handle, os.path.join(seg_path, f'seg_{dcmfolder}.nii.gz'))
+    sitk.WriteImage(sitk.GetImageFromArray(pancreas_numpy), os.path.join(pancreas_seg_path, f'pancreas_seg_{dcmfolder}.nii.gz'))
+    sitk.WriteImage(sitk.GetImageFromArray(tumor_numpy), os.path.join(tumor_seg_path, f'tumor_seg_{dcmfolder}.nii.gz'))
+    sitk.WriteImage(sitk.GetImageFromArray(ctv_numpy), os.path.join(ctv_seg_path, f'ctv_seg_{dcmfolder}.nii.gz'))
 
 logger.info(f"no_pancreas = {no_pancreas}")
 logger.info(f"mo_tumor = {no_tumor}")
 logger.info(f"no_ctv = {no_ctv}")
+logger.info(f"who_has_tiny_pancreas={who_has_tiny_pancreas}")
